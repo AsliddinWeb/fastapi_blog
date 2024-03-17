dee3183af3e1ed8b24ee4b9646a6b62944f1b33c
AsliddinWeb <asliddinsinger@gmail.com>
2024-03-17 22:59:31 +0500

    Added docker

--- main.py
+++ main.py
@@ -2,10 +2,22 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get('/')
+@app.get('/blog')
 def index():
-    return {'data': {'name': 'Asliddin'}}
+    return {'data': 'Blog list!'}
 
-@app.get('/about')
-def about():
-    return {'data': 'About page!'}
+@app.get('/blog/unpublished')
+def unpublished():
+    return {
+        'data': 'All unpublished blogs!'
+    }
+
+@app.get('/blog/{id}')
+def show(id: int):
+    return {'data': id}
+
+@app.get('/blog/{id}/comments')
+def comments(id: int):
+    return {
+        'data': {1, 2},
+    }
