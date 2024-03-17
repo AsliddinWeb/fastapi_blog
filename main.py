be8a4671afe51eae20db73651689288ebdc46a77
AsliddinWeb <asliddinsinger@gmail.com>
2024-03-18 00:06:44 +0500

    Data updated

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI
 
+from typing import Optional
+
 app = FastAPI()
 
 @app.get('/blog')
-def index():
-    return {'data': 'Blog list!'}
+def index(limit = 10, published: bool = True, sort: Optional[str] = None):
+    if published:
+        return {'data': f'{limit} published blog db!'}
+    else:
+        return {'data': f'{limit} blog db!'}
 
 @app.get('/blog/unpublished')
 def unpublished():
